0483a41848e8642f138b80d9055455f5ca5a724a
aguerokhun <sanusitaiwo10@gmail.com>
2022-12-08 19:12:35 +0000

    mike

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
+/**
+ *main - entry point
+ *
+ *Description: 'prints a file'
+ *
+ *Return: Always 0 (success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
