cdd7567298c67a0589bf3b9bcc7d75c3fd6d6f5d
aguerokhun <sanusitaiwo10@gmail.com>
2022-12-08 19:53:23 +0000

    mike

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int integerType;
+	float floatType;
+	long longintType:
+	char charType;
+	long longlongintType;
+
+	/*Sizeof operator is used to evaluate the size of a variable*/
+	printf("Size of char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of int: %ld byte(s)\n", sizeof(integerType));
+	printf("Size of long int: %ld byte(s)\n", sizeof(longType));
+	printf("Size of long long int: %ld byte(s)\n", sizeof(longType));
+	printf("Size of float: %ld byte(s)\n", sizeof(floatType);
+	return (0);
+}
